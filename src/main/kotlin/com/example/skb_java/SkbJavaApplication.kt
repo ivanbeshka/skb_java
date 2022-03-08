c42652966f20636d4cb53042faea507cadd24ccd
Ваня <ivanbeshka2002@gmail.com>
2022-03-08 18:42:45 +0500

    Initial commit

---
+++ src/main/kotlin/com/example/skb_java/SkbJavaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.skb_java
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkbJavaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkbJavaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/skb_java/SkbJavaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.skb_java
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkbJavaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
